7fa97da96f7f8debcaf62e5367a9ab26ed6ef37f
crayolito <josalejandroapp2017@gmail.com>
2024-06-24 07:42:26 -0400

    UPDATE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component'),
+  },
+  {
+    path: 'politicas-empresa',
+    loadComponent: () =>
+      import('./politicas/politicas.component'),
+  },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: 'home', pathMatch: 'full'
+  }
+];
