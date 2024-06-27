3819167ab8db66ac90d150a96db36f470dc11f9e
crayolito <josalejandroapp2017@gmail.com>
2024-06-27 13:38:36 -0400

    hoy final

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(RecaptchaV3Module),
+  { provide: RECAPTCHA_V3_SITE_KEY, useValue: "6LfL1gIqAAAAAMrVXVPLVZ8DlDLK-3yQ8B1Luxhu" }]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { RecaptchaModule, ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule, RecaptchaModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export default class HomeComponent {
+  // export class ModalComponent {
+  //   constructor(private el: ElementRef) {}
 
+  //   // Escucha clics en el documento
+  //   @HostListener('document:click', ['$event'])
+  //   onDocumentClick(event: MouseEvent): void {
+  //     // Verifica si el clic fue fuera del elemento del modal
+  //     if (!this.el.nativeElement.contains(event.target)) {
+  //       this.closeModal();
+  //     }
+  //   }
+
+  //   closeModal(): void {
+  //     // Aquí tu lógica para cerrar el modal, por ejemplo, cambiar una variable de estado
+  //     console.log('Modal cerrado');
+  //   }
+  // }
+  public showModal = true;
+  public reCaptchaV3Service = inject(ReCaptchaV3Service)
+
+  executeReCaptchaV3(): void {
+    console.log("object");
+    this.reCaptchaV3Service.execute('')
+      .subscribe((token) => {
+        console.log(token);
+      });
+  }
+
+  executeRecaptchaVisible(token: any): void {
+    console.log(token);
+
+  }
+
+  continuar(): void {
+    this.showModal = false;
+  }
+
+  rechazar(): void {
+    this.showModal = false;
+  }
 }
